40458a6488ac9cc6bb789f21befddd65849d6a81
Aditya kumar <91314940+harshitaditya@users.noreply.github.com>
2024-04-05 12:42:30 +0530

    Time: 3 ms (61.07%), Space: 42.4 MB (39.72%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,12 +1,20 @@
-//using recursion
+//using stack
 class Solution {
     public String makeGood(String s) {
-        for(int i=0;i<s.length()-1;i++){
-            if(s.charAt(i)+32==s.charAt(i+1)||s.charAt(i)==s.charAt(i+1)+32){
-                return makeGood(s.substring(0,i)+s.substring(i+2));
-            }
+        Stack<Character> stack=new Stack();
+        for(char ch:s.toCharArray()){
+            if(!stack.isEmpty() && Math.abs(stack.peek() - ch)==32)
+                stack.pop();
+            else
+                stack.add(ch);
+            
         }
-        return s;
+        StringBuffer sb=new StringBuffer();
+        while(stack.size()>0){
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+        
         
     }
 }
